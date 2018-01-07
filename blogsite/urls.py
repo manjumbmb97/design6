779408ec804125a6a7171a17213e24ea6c83348d
manjumbmb97 <manjumbmb97@gmail.com>
2018-01-07 18:17:15 +0530

    another commit

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^password-change/$', views.password_change, name='password_change'),
+    url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
     url(r'', include('design6.urls')),
 ]
