5306b9d9204e89e7c613bbde79e130151a07f356
Manjunath Maigur <manjumbmb97@gmail.com>
2018-01-31 23:04:56 +0530

    commit for heroku

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -36,6 +36,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
